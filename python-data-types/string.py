7c4d4fd0603fb24d15b34b032092faa8fdc886b9
Chirag-savage <chirag1212kumargmail.com>
2022-03-26 10:20:29 +0530

    python data types

--- python-data-types/string.py
+++ python-data-types/string.py
@@ -8,9 +8,16 @@ print(name[1])
 print(name[-1])
 
 
-#slicing a string
+#FORMATTED STRINGS
+first = 'john'
+last = 'smith'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+
+#STRING SLICING
 # --simple example
 print(name[1:3])
+print(name[1:-1])
 # -- slicing that have no starting index but have an ending index
 print(name[:4])
 # --slicing that have a starting index but not have an ending index
@@ -19,7 +26,7 @@ print(name[2:])
 print(name[::-1])
 
 
-#built-in function for strings
+#STRING METHODS
 
 print(name.capitalize()) #capitalise the first character of string
 print(name.islower())  #check if there atleast one lowercase letter
@@ -27,7 +34,10 @@ print(name.isupper())  #che3ck if there is atleast one uppercase letter
 print(name.isalpha())  #check if there is alphabetical character or not
 print(name.isalnum())  #check i fthere are alphabet or numeric character in string
 print(name.swapcase())  #swap the case of every character in string
+print(name.find('h'))
+print(name.find('ello'))
+print(name.replace('h','k'))
+
+#LEN FUNTION
 print(len(name)) #find the lenght of the strings
-
-
 
