878a82440155e8acd3499e19eeb802101bc7a826
Chirag-savage <chirag1212kumargmail.com>
2022-03-28 14:24:44 +0530

    tuple updated

---
+++ python-data-types/tuple.py
@@ -0,0 +1,6 @@
+tuple = (123,'hello')
+tuple1 = ('world')
+print(tuple) #will output whole tuple. (123,'hello')
+print(tuple[0]) #will output first value. (123)
+print(tuple + tuple1) #will output (123,'hello','world')
+tuple[1]='update' #this will give you error.
