7a81b1d09902eac216c39bb25147b31f89c573fa
Chirag-savage <chirag1212kumargmail.com>
2022-03-28 14:20:29 +0530

    list updated

--- python-data-types/list.py
+++ python-data-types/list.py
@@ -1,4 +1,15 @@
 list_1 = [1,2,3,4,4,5]
+list_2 = ['a','hello','world']
+
+#PRINTING LIST
 print(list_1)
 
+#LIST SLICING
 print(list_1[0:2])
+
+
+#LIST REPLICATION
+print(list_1*2)
+
+#LIST CONCATENATION
+print(list_1+list_2)
