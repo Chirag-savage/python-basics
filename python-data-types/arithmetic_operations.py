5bcfedaae0cff723fab01672c4685582cedb71bf
Chirag-savage <chirag1212kumargmail.com>
2022-03-29 19:50:44 +0530

    updated

---
+++ python-data-types/arithmetic_operations.py
@@ -0,0 +1,27 @@
+#BASIC ,ATHS OPERATOR IN PT\YTHON
+
+print(10+3)
+print(10*3)
+print(12/3)
+print(108-98)
+print(12//3)
+print(14%3)
+print(2**3)
+
+#AUGMENTED ASSIGNMENT OPERATOR
+
+var_1 = 167
+var_1 += 101
+print(var_1)
+var_1 -= 4
+print(var_1)
+var_1 *= 4
+print(var_1)
+var_1 /= 4
+print(var_1)
+
+
+#OPERATOR PREECENDENCE IN PYTHON
+
+x= (10%4) + 4**2 +6
+print(x)
